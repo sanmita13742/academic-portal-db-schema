fc76fe52d46b6e0226e239532aed3b70ba35dc90
tishtakalita <tishtakalita@gmail.com>
2025-07-16 21:46:59 +0530

    Update timetable.sql

--- schemas/timetable.sql
+++ schemas/timetable.sql
@@ -1,1 +1,18 @@
+--timetables table
+CREATE TABLE timetables (
+    timetable_id INT AUTO_INCREMENT PRIMARY KEY,
+    slot_no INT NOT NULL,
+    start_time TIME NOT NULL,
+    end_time TIME NOT NULL,
+    day ENUM('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday') NOT NULL,
+    room VARCHAR(50),
+    teacher_id INT NOT NULL,
+    course_id INT NOT NULL,
+    class_sec INT NOT NULL,
+
+    -- Foreign Key Constraints
+    FOREIGN KEY (course_id) REFERENCES courses(course_id),
+    FOREIGN KEY (class_sec) REFERENCES class(class_id),
+    FOREIGN KEY (teacher_id) REFERENCES users(user_id)
+);
 
